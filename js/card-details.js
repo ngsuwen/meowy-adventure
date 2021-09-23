35e93856dad98426bc6445ce7def6530135c81af
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-09-24 02:40:02 +0800

    card description window popups on click and hold

--- js/card-details.js
+++ js/card-details.js
@@ -1,16 +1,19 @@
 $(()=>{
-    $('.open-gallery-link').mouseover(function() {
+    $('.open-gallery-link').mousedown(function() {
         var items = [];
         $( $(this).attr('href') ).find('.card-detail').each(function() {
           items.push( {
             src: $(this) 
           } );
         });
-        $.magnificPopup.open({
-          items:items,
-          gallery: {
-            enabled: true 
-          }
+        let timeout_id=setTimeout(function(){
+            $.magnificPopup.open({
+            items:items,
+            gallery: {
+                enabled: true 
+            } 
         });
+    },1000)
+    $('.open-gallery-link').bind('mouseup mouseleave', function(){clearTimeout(timeout_id)})
     });
 })
