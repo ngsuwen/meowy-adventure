2d557f6ccdfe95178209743ddcf7b4b1614d945d
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2021-09-26 16:17:41 +0800

    added css for dialogs

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,16 +1,21 @@
 $(()=>{
     $('.popup-link').magnificPopup({
-      closeBtnInside:true
+      closeBtnInside:false
     });
 
     // opens tutorial on load
     $.magnificPopup.open({
-            items:{
-                src: '#first-popup',
-                type: 'inline'
-            },
-            gallery: {
-                enabled:true
-            }
+        items:{
+            src: '#first-popup',
+            type: 'inline',
+        },
+        gallery: {
+            enabled:true
+        },
+        closeBtnInside:false
     })
+
+    $('#close-button').click(function(){
+        $.magnificPopup.close();
+     });
 })
